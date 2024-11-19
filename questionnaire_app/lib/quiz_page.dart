a73bcc578d5499af552133ec2b2f430ba6a8e843
MERRET Loic <Loic.Merret@bordeaux-inp.fr>
2024-11-19 10:51:21 +0100

    questions frises chronologiques

--- questionnaire_app/lib/quiz_page.dart
+++ questionnaire_app/lib/quiz_page.dart
@@ -49,6 +49,55 @@ class _QuizPageState extends State<QuizPage> {
     _loadQuestions(); // Charger les questions au démarrage
   }
 
+  Future<void> _showFeedbackAnimation(bool isCorrect, String correctAnswer) async {
+  final Color color = isCorrect ? Colors.green : Colors.red;
+  final IconData icon = isCorrect ? Icons.check_circle : Icons.cancel;
+  final String message = isCorrect 
+      ? "Bonne réponse !" 
+      : "Mauvaise réponse\nLa bonne réponse était : $correctAnswer";
+
+  await showDialog(
+    context: context,
+    barrierDismissible: true, 
+    builder: (BuildContext context) {
+      return Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 80, color: Colors.white),
+              const SizedBox(height: 20),
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    },
+  );
+  _nextQuestion();
+}
+
+
+
+
   Widget _printQuestionAnswers(
       Map<String, dynamic> currentQuestion, List<String> options) {
     // Afficher les réponses sous forme de boutons
@@ -138,6 +187,54 @@ class _QuizPageState extends State<QuizPage> {
             );
           }).toList(),
         );
+        case "timeline":
+          double selectedYear = double.parse(currentQuestion['options'][0]);
+          double minYear = double.parse(currentQuestion['options'][0]);
+          double maxYear = double.parse(currentQuestion['options'][1]);
+
+          return StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                children: [
+                  // Frise chronologique avec le Slider
+                  Slider(
+                    value: selectedYear,
+                    min: minYear,
+                    max: maxYear,
+                    divisions: (maxYear - minYear).toInt(),
+                    label: selectedYear.toInt().toString(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedYear = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  // Afficher l'année sélectionnée
+                  Text(
+                    'Année sélectionnée : ${selectedYear.toInt()}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 20),
+                  // Bouton de validation
+                  ElevatedButton(
+                    onPressed: _selectedAnswer.isEmpty
+                        ? () {
+                            _checkAnswer(
+                                selectedYear.toInt().toString(),
+                                currentQuestion['correct_answer']);
+                          }
+                        : null,
+                    child: const Text(
+                      'Valider',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+
       default:
         throw Exception("Type de question inconnu");
     }
@@ -161,15 +258,19 @@ class _QuizPageState extends State<QuizPage> {
     });
   }
 
-  void _checkAnswer(String selectedOption, String correctAnswer) {
-    setState(() {
-      if (selectedOption == correctAnswer) {
-        _correctAnswersCount++; // Incrémenter le nombre de bonnes réponses
-      }
-      _selectedAnswer =
-          selectedOption == correctAnswer ? 'correct' : 'incorrect';
-    });
-  }
+    void _checkAnswer(String selectedOption, String correctAnswer) async {
+      setState(() {
+        if (selectedOption == correctAnswer) {
+          _correctAnswersCount++; // Incrémenter le nombre de bonnes réponses
+          _selectedAnswer = 'correct';
+        } else {
+          _selectedAnswer = 'incorrect';
+        }
+      });
+
+      // Appeler l'animation de feedback
+      await _showFeedbackAnimation(_selectedAnswer == 'correct', correctAnswer);
+    }
 
   @override
   Widget build(BuildContext context) {
